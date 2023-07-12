a2f03fdce41f2e202e9a38e3944865e9fc7ab37b
Harwin14 <agungharwinn@gmail.com>
2023-07-12 20:26:30 +0700

    initial project

---
+++ src/views/Home.vue
@@ -0,0 +1,100 @@
+<template>
+  <Navbar />
+
+  <v-container fluid>
+    <v-row>
+      <v-col cols="22">
+        <v-card class="mx-auto rounded-xl" flat>
+          <v-img src="cover2.png" max-height="100%" max-width="100%" contain>
+            <v-card-title class="top ml-10">
+              <h2 class="title1">Order Your</h2>
+            </v-card-title>
+            <v-card-title class="ml-10 mt-n18">
+              <h2 class="title2">Daily Groceries</h2>
+            </v-card-title>
+            <v-card-text class="ml-10">
+              <h2 class="green--text">#Free Delivery</h2>
+            </v-card-text>
+            <v-card-text class="ml-10">
+                <v-row>
+                  <v-col cols="4">
+                    <v-text-field
+                      label="Search your daily groceries"
+                      class="d-none d-sm-none d-md-flex"
+                      filled
+                      prepend-inner-icon="mdi-magnify"
+                      solo
+                      flat
+                      background-color=""
+                      rounded
+                      outlined
+                      success
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="2" class="marginLeft">
+                    <v-btn
+                      color="green"
+                      rounded
+                      dark
+                      x-large
+                      class="py-7 d-none d-sm-none d-md-flex"
+                    >
+                      search
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+</script>
+<style>
+.marginLeft {
+  margin-left: -90px;
+}
+.mtop {
+  margin-top: 100px;
+}
+.mbottom {
+  margin-bottom: 100px;
+}
+.v-card.borderme {
+  border: 2px solid green !important;
+}
+.v-card.borderout {
+  border: 1px solid #d5f0db !important;
+}
+.v-btn:not(.v-btn--round).v-size--default.add {
+  min-width: 0px !important;
+}
+.theme--light.v-sheet--outlined.mobile {
+  border: 2px solid black !important;
+}
+@media only screen and (max-width: 600px) {
+  h2.title1 {
+    font-size: 15px;
+  }
+  h2.title2 {
+    font-size: 15px;
+  }
+  .top {
+    margin-top: 20px;
+  }
+}
+@media only screen and (min-width: 600px) {
+  .top {
+    margin-top: 70px;
+  }
+}
+@media only screen and (min-width: 768px) {
+  .top {
+    margin-top: 120px;
+  }
+}
+</style>
